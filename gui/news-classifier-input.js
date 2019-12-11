258c04f6cc0745277e1aa0cb7ac905002216ec9e
aljubaer <bsse0812@iit.du.ac.bd>
2019-12-11 15:12:48 +0600

    topic distribution ui done

--- gui/news-classifier-input.js
+++ gui/news-classifier-input.js
@@ -27,6 +27,52 @@ function onDataInput() {
             console.log(`statusCode: ${res.statusCode}`);
             var newBody = body.replace(/'/g, '\"');
             let result = JSON.parse(newBody);
-            
+            console.log(result);
+            textStatus.innerText = ''
+            textResult.innerText = 'খেলা' + ' :' +  result['খেলা'] + '%\t'
+                                    + 'রাজনীতি' + ' :' +  result['রাজনীতি'] + '%\n'
+                                    + 'অপরাধ' + ' :' +  result['অপরাধ'] + '%\t'
+                                    + 'আন্তর্জাতিক' + ' :' +  result['আন্তর্জাতিক'] + '%\n'
+                                    + 'বিনোদন' + ' :' +  result['বিনোদন'] + '%\t'
+                                    + 'বিজ্ঞান ও প্রযুক্তি' + ' :' +  result['বিজ্ঞান ও প্রযুক্তি'] + '%\n';
+
+            var ctx = document.getElementById('myChart');
+            var myChart = new Chart(ctx, {
+                type: 'radar',
+                data: {
+                    labels: ['খেলা', 'রাজনীতি', 'অপরাধ', 'আন্তর্জাতিক', 'বিনোদন', 'বিজ্ঞান ও প্রযুক্তি'],
+                    datasets: [{
+                        label: '# topic distribution',
+                        data: [result['খেলা'], result['রাজনীতি'], result['অপরাধ'], 
+                        result['আন্তর্জাতিক'], result['বিনোদন'], result['বিজ্ঞান ও প্রযুক্তি']],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
     });
 }
